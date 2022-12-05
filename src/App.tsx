7185b3ff607bff8ede807067365e41c130258880
JOSE LUIZ CABOCLO DOS SANTOS <112710477+luizcaboclo@users.noreply.github.com>
2022-12-05 12:42:52 -0300

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,42 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Navbar from './components/estaticos/navbar/Navbar';
+import Home from './paginas/home/Home'
+import Footer from './components/estaticos/footer/Footer';
+import {BrowserRouter as Router, Route,Routes} from 'react-router-dom'
+import Login from './paginas/login/Login'
+import CadastroUsuario from './paginas/cadastroUsuario/CadastroUsuario';
+import ListaPostagem from './components/postagem/listapostagem/ListaPostagem';
+import ListaTema from './components/temas/listatemas/ListaTemas';
+import CadastroPost from './components/postagem/cadastroPost/CadastroPost';
+import DeletarPostagem from './components/postagem/deletarPostagens/DeletarPostagens';
+import DeletarTema from './components/temas/deletarTema/DeletarTema';
+import CadastroTema from './components/temas/cadastroTema/CadastroTema';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <Router>
+      <Navbar />
+       <Routes>
+        <Route path='/' element={<Login/>}/>
+        <Route path='/login' element={<Login />} />
+        <Route path='/home' element={<Home/>} />
+        <Route path='/cadastrousuario' element={<CadastroUsuario />}/>
+        <Route path='/temas' element={<ListaTema />} />
+        <Route path='/postagem' element={<ListaPostagem />} />
+        <Route path="/formularioPostagem" element={<CadastroPost />} />
+        <Route path="/formularioPostagem/:id" element={<CadastroPost />} />
+        <Route path="/formularioTema" element={<CadastroTema />} />
+        <Route path="/formularioTema/:id" element={<CadastroTema />} />
+        <Route path="/deletarPostagem/:id" element={<DeletarPostagem />} />
+        <Route path="/deletarTema/:id" element={<DeletarTema />} />
+       </Routes>
+       <Footer />
+    </Router>
+    </>
   );
 }
 
